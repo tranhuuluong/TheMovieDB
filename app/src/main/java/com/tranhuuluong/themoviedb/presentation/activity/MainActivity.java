eee44cd8190abacaf2274c6c745272ccc1a690f2
Luong <luong.tran@2359media.com>
2018-09-25 17:13:02 +0700

    [ADD] init project

---
+++ app/src/main/java/com/tranhuuluong/themoviedb/presentation/activity/MainActivity.java
@@ -0,0 +1,15 @@
+package com.tranhuuluong.themoviedb.presentation.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.tranhuuluong.themoviedb.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
